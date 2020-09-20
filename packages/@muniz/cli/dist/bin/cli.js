4ae800fcb99ce8cb3dfefc2a8c6ee33b2fb31f12
1160007652 <1160007652@qq.com>
2020-09-20 21:49:40 +0800

    feat(动态加载使用插件): 因为通过babel解析,所以使用 import() 动态加载已安装的插件
    affects: @muniz/cli
    
    import 动态加载插件，执行插件

--- packages/@muniz/cli/dist/bin/cli.js
+++ packages/@muniz/cli/dist/bin/cli.js
@@ -3,6 +3,12 @@
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
+var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
+
+var _interopRequireWildcard2 = _interopRequireDefault(require("@babel/runtime/helpers/interopRequireWildcard"));
+
+var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
+
 var _react = _interopRequireDefault(require("react"));
 
 var _ink = require("ink");
@@ -10,8 +16,6 @@ var _ink = require("ink");
 var _meow = _interopRequireDefault(require("meow"));
 
 var _ui = require("../ui");
-
-var _munizPluginI18n = _interopRequireDefault(require("@muniz/muniz-plugin-i18n"));
 
 var program = (0, _meow["default"])({
   flags: {
@@ -26,44 +30,93 @@ var program = (0, _meow["default"])({
   },
   autoHelp: false
 });
-/**
- * @description 输入命令小于 2 位,打印 使用帮助文档
- */
 
-if (!process.argv.slice(2).length) {
-  // program.showHelp();
-  (0, _ink.render)( /*#__PURE__*/_react["default"].createElement(_ui.UI_Command, program.flags));
-}
-
-var input = program.input,
-    flags = program.flags;
-
-if (input.length === 0 && flags.help) {
-  (0, _ink.render)( /*#__PURE__*/_react["default"].createElement(_ui.UI_Command, program.flags));
-  process.exit();
-}
-
-if (input.length > 0 && flags.help) {
-  console.log('打印子命令 help 文档');
-  process.exit();
-}
-
-if (input.length > 0 && !flags.help) {
-  var commander = {
-    create: function create(options) {
-      (0, _ink.render)( /*#__PURE__*/_react["default"].createElement(_ui.UI_Create, options));
-    },
-    add: function add() {
-      console.log('安装插件');
-    }
-  }; // 执行子命令
-
-  if ([input[0]] in commander) {
-    commander === null || commander === void 0 ? void 0 : commander[input[0]]({
-      input: input,
-      flags: flags
-    });
-  } else {
-    console.log("\u4E0D\u5B58\u5728\u547D\u4EE4 ".concat(input[0]));
-  }
+function _start() {
+  return _start2.apply(this, arguments);
 } // console.log(cli);
+
+
+function _start2() {
+  _start2 = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee() {
+    var input, flags, commander, _yield$import, plugin;
+
+    return _regenerator["default"].wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            /**
+             * @description 输入命令小于 2 位,打印 使用帮助文档
+             */
+            if (!process.argv.slice(2).length) {
+              // program.showHelp();
+              (0, _ink.render)( /*#__PURE__*/_react["default"].createElement(_ui.UI_Command, program.flags));
+            }
+
+            input = program.input, flags = program.flags;
+
+            if (input.length === 0 && flags.help) {
+              (0, _ink.render)( /*#__PURE__*/_react["default"].createElement(_ui.UI_Command, program.flags));
+              process.exit();
+            }
+
+            if (input.length > 0 && flags.help) {
+              console.log('打印子命令 help 文档');
+              process.exit();
+            }
+
+            if (!(input.length > 0 && !flags.help)) {
+              _context.next = 21;
+              break;
+            }
+
+            commander = {
+              create: function create(options) {
+                (0, _ink.render)( /*#__PURE__*/_react["default"].createElement(_ui.UI_Create, options));
+              },
+              add: function add() {
+                console.log('安装插件');
+              }
+            }; // 执行子命令
+
+            if (!([input[0]] in commander)) {
+              _context.next = 10;
+              break;
+            }
+
+            commander === null || commander === void 0 ? void 0 : commander[input[0]]({
+              input: input,
+              flags: flags
+            });
+            _context.next = 21;
+            break;
+
+          case 10:
+            _context.prev = 10;
+            _context.next = 13;
+            return Promise.resolve("@muniz/muniz-plugin-".concat(input[0])).then(function (s) {
+              return (0, _interopRequireWildcard2["default"])(require(s));
+            });
+
+          case 13:
+            _yield$import = _context.sent;
+            plugin = _yield$import["default"];
+            console.log(plugin);
+            _context.next = 21;
+            break;
+
+          case 18:
+            _context.prev = 18;
+            _context.t0 = _context["catch"](10);
+            console.log("\u4E0D\u5B58\u5728\u547D\u4EE4 ".concat(input[0]));
+
+          case 21:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee, null, [[10, 18]]);
+  }));
+  return _start2.apply(this, arguments);
+}
+
+_start();
--- packages/@muniz/cli/src/bin/cli.js
+++ packages/@muniz/cli/src/bin/cli.js
@@ -7,8 +7,6 @@ import meow from 'meow';
 
 import { UI_Command, UI_Create } from '../ui';
 
-import i18n from '@muniz/muniz-plugin-i18n';
-
 const program = meow({
   flags: {
     help: {
@@ -23,41 +21,50 @@ const program = meow({
   autoHelp: false,
 });
 
-/**
- * @description 输入命令小于 2 位,打印 使用帮助文档
- */
-if (!process.argv.slice(2).length) {
-  // program.showHelp();
-  render(React.createElement(UI_Command, program.flags));
-}
+async function _start() {
+  /**
+   * @description 输入命令小于 2 位,打印 使用帮助文档
+   */
+  if (!process.argv.slice(2).length) {
+    // program.showHelp();
+    render(React.createElement(UI_Command, program.flags));
+  }
 
-const { input, flags } = program;
+  const { input, flags } = program;
 
-if (input.length === 0 && flags.help) {
-  render(React.createElement(UI_Command, program.flags));
-  process.exit();
-}
+  if (input.length === 0 && flags.help) {
+    render(React.createElement(UI_Command, program.flags));
+    process.exit();
+  }
 
-if (input.length > 0 && flags.help) {
-  console.log('打印子命令 help 文档');
-  process.exit();
-}
+  if (input.length > 0 && flags.help) {
+    console.log('打印子命令 help 文档');
+    process.exit();
+  }
 
-if (input.length > 0 && !flags.help) {
-  const commander = {
-    create: (options) => {
-      render(React.createElement(UI_Create, options));
-    },
-    add: () => {
-      console.log('安装插件');
-    },
-  };
+  if (input.length > 0 && !flags.help) {
+    const commander = {
+      create: (options) => {
+        render(React.createElement(UI_Create, options));
+      },
+      add: () => {
+        console.log('安装插件');
+      },
+    };
 
-  // 执行子命令
-  if ([input[0]] in commander) {
-    commander?.[input[0]]({ input, flags });
-  } else {
-    console.log(`不存在命令 ${input[0]}`);
+    // 执行子命令
+    if ([input[0]] in commander) {
+      commander?.[input[0]]({ input, flags });
+    } else {
+      try {
+        // 使用插件命令
+        const { default: plugin } = await import(`@muniz/muniz-plugin-${input[0]}`);
+        console.log(plugin);
+      } catch {
+        console.log(`不存在命令 ${input[0]}`);
+      }
+    }
   }
 }
 // console.log(cli);
+_start();
