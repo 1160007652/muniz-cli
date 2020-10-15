d4b264157c6729f82bc80a8891fedc24e698aeed
muniz <1160007652@qq.com>
2020-10-15 10:20:08 +0800

    feat(优化架构): 优化组织架构
    affects: @muniz/cli-shared-utils, @muniz/cli, @muniz/ink-ui, @muniz/muniz-plugin-create,
    @muniz/muniz-plugin-i18n
    
    划分不同的服务区域

--- packages/@muniz/ink-ui/dist/index.js
+++ packages/@muniz/ink-ui/dist/index.js
@@ -1,31 +1,31 @@
-'use strict';
+"use strict";
 
-var _interopRequireDefault = require('@babel/runtime/helpers/interopRequireDefault');
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
-Object.defineProperty(exports, '__esModule', {
-  value: true,
+Object.defineProperty(exports, "__esModule", {
+  value: true
 });
-Object.defineProperty(exports, 'TextInput', {
+Object.defineProperty(exports, "TextInput", {
   enumerable: true,
   get: function get() {
-    return _TextInput['default'];
-  },
+    return _TextInput["default"];
+  }
 });
-Object.defineProperty(exports, 'Button', {
+Object.defineProperty(exports, "Button", {
   enumerable: true,
   get: function get() {
-    return _Button['default'];
-  },
+    return _Button["default"];
+  }
 });
-Object.defineProperty(exports, 'ButtonGroup', {
+Object.defineProperty(exports, "ButtonGroup", {
   enumerable: true,
   get: function get() {
-    return _ButtonGroup['default'];
-  },
+    return _ButtonGroup["default"];
+  }
 });
 
-var _TextInput = _interopRequireDefault(require('./components/TextInput'));
+var _TextInput = _interopRequireDefault(require("./components/TextInput"));
 
-var _Button = _interopRequireDefault(require('./components/Button'));
+var _Button = _interopRequireDefault(require("./components/Button"));
 
-var _ButtonGroup = _interopRequireDefault(require('./components/ButtonGroup'));
+var _ButtonGroup = _interopRequireDefault(require("./components/ButtonGroup"));
--- packages/@muniz/muniz-plugin-i18n/dist/index.js
+++ packages/@muniz/muniz-plugin-i18n/dist/index.js
@@ -1,38 +1,39 @@
-'use strict';
+"use strict";
 
-var _interopRequireDefault = require('@babel/runtime/helpers/interopRequireDefault');
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
-Object.defineProperty(exports, '__esModule', {
-  value: true,
+Object.defineProperty(exports, "__esModule", {
+  value: true
 });
-Object.defineProperty(exports, 'i18n', {
+Object.defineProperty(exports, "i18n", {
   enumerable: true,
   get: function get() {
     return _i18n.i18n;
-  },
+  }
 });
-Object.defineProperty(exports, 'initI18nLocales', {
+Object.defineProperty(exports, "initI18nLocales", {
   enumerable: true,
   get: function get() {
     return _i18n.initI18nLocales;
-  },
+  }
 });
-exports['default'] = void 0;
+exports["default"] = void 0;
 
-var _configs = _interopRequireDefault(require('./configs'));
+var _configs = _interopRequireDefault(require("./configs"));
 
-var _Switch = _interopRequireDefault(require('./ui/Switch'));
+var _Switch = _interopRequireDefault(require("./ui/Switch"));
 
-var _List = _interopRequireDefault(require('./ui/List'));
+var _List = _interopRequireDefault(require("./ui/List"));
 
-var _i18n = require('./lib/i18n');
+var _i18n = require("./lib/i18n");
 
 var _default = {
-  config: _configs['default'],
+  config: _configs["default"],
   command: {
-    switch: _Switch['default'],
-    list: _List['default'],
-    default: _Switch['default'], // 默认执行的命令
-  },
+    "switch": _Switch["default"],
+    list: _List["default"],
+    "default": _Switch["default"] // 默认执行的命令
+
+  }
 };
-exports['default'] = _default;
+exports["default"] = _default;
