1fcd8043b3e209ccaed3c2ff6f8a3f24e1b41737
muniz <1160007652@qq.com>
2020-10-29 14:58:38 +0800

    feat(升级version指令): 变更author为homepage
    affects: @muniz/cli, @muniz/ink-ui, @muniz/muniz-plugin-create
    
    变更version author

--- packages/@muniz/ink-ui/src/Version/index.js
+++ packages/@muniz/ink-ui/src/Version/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Box, Text } from 'ink';
 
 const Version = ({ pkg }) => {
-  const { version, author, name } = pkg;
+  const { version, homepage, name } = pkg;
   return (
     <Box flexDirection="column" paddingTop={1}>
       <Text>插件名称：{name}</Text>
       <Text>当前版本：{version}</Text>
-      <Text>作者：{author}</Text>
+      <Text>Git：{homepage}</Text>
     </Box>
   );
 };
