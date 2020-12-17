26626134857da68dca5d4df8abab030cc681e1ee
muniz <1160007652@qq.com>
2020-12-17 16:44:10 +0800

    feat(muniz-plugin-create): 增加extension chrome 扩展插件 ts 开发模版
    affects: @muniz/muniz-plugin-create
    
    extensions ts

--- packages/@muniz/muniz-plugin-create/src/command/Extension/prompts.js
+++ packages/@muniz/muniz-plugin-create/src/command/Extension/prompts.js
@@ -1,8 +1,15 @@
-import { prokectName, projectDesc, isGit, extensionShortName, extensionAuthor } from '../../lib/prompts';
+import { prokectName, projectDesc, isTypeScript, isGit, extensionShortName, extensionAuthor } from '../../lib/prompts';
 
 const inquirer = require('inquirer');
 
 export default async () => {
-  const answers = await inquirer.prompt([prokectName, extensionShortName, projectDesc, extensionAuthor, isGit]);
+  const answers = await inquirer.prompt([
+    prokectName,
+    extensionShortName,
+    projectDesc,
+    extensionAuthor,
+    isTypeScript,
+    isGit,
+  ]);
   return answers;
 };
